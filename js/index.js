218c5af227e8754b7f8aa60dce1ec5f4ac30b192
Elliot Sverin <elliot.sverin@hyperisland.se>
2018-01-25 13:28:12 +0100

    Bodymovin test
    Added some Bodymovin testfiles

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('bm'),
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  path: "elliotsverin.github.io/js/data.json"
+});
