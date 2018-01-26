b63aed1bb07f666a07b703b1cc98f8917e6f80b0
Elliot Sverin <elliot.sverin@hyperisland.se>
2018-01-26 09:48:07 +0100

    GSAP and Footer placement

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('bm'),
-  renderer: "svg",
-  loop: false,
-  autoplay: true,
-  path: "data.json"
+document.addEventListener("DOMContentLoaded", function(event){
+  TweenMax.from((".rocket"), 2, {autoAlpha: 0});
+  var animation = bodymovin.loadAnimation({
+    container: document.getElementById('bm'),
+    renderer: "svg",
+    loop: false,
+    autoplay: true,
+    path: "data.json"
+  });
 });
+
+
+
+
+
+/*window.onload = function() {
+  var body = documen.getElementById((#body));
+
+}
+
+*/
