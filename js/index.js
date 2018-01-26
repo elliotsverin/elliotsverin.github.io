c0364f4273b7553863fa8d75092e03a5d7b8de44
Elliot Sverin <elliot.sverin@hyperisland.se>
2018-01-26 10:41:37 +0100

    Big update!
    The footer now has the proper styling
    
    the about button triggers the rocket
    
    The rocket got an exit animation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var tl = new TimelineMax();
 
 document.addEventListener("DOMContentLoaded", function(event){
+
   tl.from(("body"), 3, {backgroundColor: "#000000", ease: Power1.easeOut})
   .from((".rocket"), 2, {autoAlpha: 0, x: "-500", y: "500", left: "40%"});
   var animation = bodymovin.loadAnimation({
@@ -11,6 +12,21 @@ document.addEventListener("DOMContentLoaded", function(event){
     path: "data.json"
   });
 });
+
+document.getElementById("aboutMe").addEventListener("click", about);
+
+function about(){
+  var width = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+var height = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+
+
+  TweenMax.to((".rocket"), 1.5, {ease: Power1.easeInOut, x: width/2, y: -height/2, autoAlpha: 0});
+}
 
 
 
