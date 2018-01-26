e32b27c6c1fd89cf13d4d28d0587b4d401e8e2a1
Elliot Sverin <elliot.sverin@hyperisland.se>
2018-01-26 16:48:09 +0100

    GUDGEMIGSTYRKA

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+var  tl = new TimelineMax();
 var width = window.innerWidth ||
   document.documentElement.clientWidth ||
   document.body.clientWidth;
@@ -12,11 +13,11 @@ var select = function(s) {
   selectAll = function(s) {
     return document.querySelectorAll(s);
   },
-  animationWindow = select('bm'),
-  animData = {
+  animationWindow = select('.bm'),
+      animData = {
     wrapper: animationWindow,
     animType: 'svg',
-    loop: true,
+    loop: false,
     prerender: true,
     autoplay: true,
     path: 'data.json',
@@ -29,34 +30,37 @@ anim = bodymovin.loadAnimation(animData);
 anim.addEventListener('DOMLoaded', onDOMLoaded);
 anim.setSpeed(1);
 
-function onDOMLoaded(e) {
-  tl = new TimelineMax();
+console.log(tl + " what " + anim);
 
-  tl.from(("body"), 3, {
-      backgroundColor: "#000000",
-      ease: Power1.easeIn
-    })
-    .from((".rocket"), 2, {
-      autoAlpha: 0,
-      x: "-500",
-      y: "500",
-      left: "40%"
-    })
+//function onDOMLoaded(e) {
+
+ console.log("WEMADEIT");
+ tl = new TimelineMax();
+
+ tl.from(("body"), 3, {
+     backgroundColor: "#000000",
+     ease: Power1.easeIn
+   })
+   .from((".rocket"), 2, {
+     autoAlpha: 0,
+     x: "-500",
+     y: "500",
+     left: "40%"
+   })
     .staggerFrom((".footer_li"), 0.5, {
       x: "-50",
       autoAlpha: 0.01
     }, 0.2)
-    .to({
+     .to({
       frame: 0
     }, 6.01, {
       frame: anim.totalFrames - 1,
       onUpdate: function() {
-        console.log("yehaw");
-        anim.goToAndStop(Math.round(this.target.frame), true);
+        anim.goToAndStop(Math.round(this.target.frame), true)
       },
       ease: Linear.easeNone
-    });
-}
+    });*/
+//}
 document.addEventListener("DOMContentLoaded", function(event) {
   //var scene = document.getElementsByClassName('rocket');
   //var parallaxInstance = new Parallax(scene);
