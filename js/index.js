5b5621a7e599d8b720014929e480f9b37cb8ef22
Elliot Sverin <elliot.sverin@hyperisland.se>
2018-01-26 14:34:50 +0100

    Random changes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 var tl = new TimelineMax();
 
+var width = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+var height = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+
+
 var select = function(s) {
     return document.querySelector(s);
   },
@@ -27,16 +36,21 @@ var select = function(s) {
 	anim = bodymovin.loadAnimation(animData);
   anim.setSpeed(1);
 
+
 document.addEventListener("DOMContentLoaded", function(event){
+  //var scene = document.getElementsByClassName('rocket');
+  //var parallaxInstance = new Parallax(scene);
 
   tl.from(("body"), 3, {backgroundColor: "#000000", ease: Power1.easeIn})
-  .to({frame:0}, 6, {
+  .from((".rocket"), 2, {autoAlpha: 0, x: "-500", y: "500", left: "40%"})
+  .staggerFrom((".footer_li"), 0.5, {x: "-50", autoAlpha: 0.01}, 0.2)
+  .to({frame:0}, 6.01, {
     frame: anim.totalFrames-1,
     onUpdate:function(){
       anim.goToAndStop(Math.round(this.target.frame), true)
     }, ease:Linear.easeNone
-  })
-  .from((".rocket"), 2, {autoAlpha: 0, x: "-500", y: "500", left: "40%"});
+  });
+console.log(anim.totalFrames);
   /*var animation = bodymovin.loadAnimation({
     container: document.getElementById('bm'),
     renderer: "svg",
@@ -46,20 +60,24 @@ document.addEventListener("DOMContentLoaded", function(event){
   });*/
 });
 
+
+
 document.getElementById("aboutMe").addEventListener("click", about);
 
 function about(){
-  var width = window.innerWidth
+  width = window.innerWidth
 || document.documentElement.clientWidth
 || document.body.clientWidth;
 
-var height = window.innerHeight
+height = window.innerHeight
 || document.documentElement.clientHeight
 || document.body.clientHeight;
 
 
-  TweenMax.to((".rocket"), 1.5, {ease: Power1.easeInOut, x: width/2, y: -height/2, autoAlpha: 0});
+  TweenMax.to(("rocket"), 1.5, {ease: Power1.easeInOut, x: width/2, y: -height/1.8, autoAlpha: 0});
 }
+
+
 
 
 
