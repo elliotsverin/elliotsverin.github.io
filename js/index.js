3ddb09663a5c743b349967689ba8a4bd604c6c70
Elliot Sverin <elliot.sverin@hyperisland.se>
2018-02-12 10:57:16 +0100

    Beautification of JS and CSS

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 var  tl = new TimelineMax();
 
 
-var homeTest = "WHATUP";
 var width = window.innerWidth ||
   document.documentElement.clientWidth ||
   document.body.clientWidth;
@@ -9,11 +8,6 @@ var width = window.innerWidth ||
 var height = window.innerHeight ||
   document.documentElement.clientHeight ||
   document.body.clientHeight;
-
-
-//console.log(tl + " what " + anim);
-
-//function onDOMLoaded(e) {
 
  var anim = bodymovin.loadAnimation({
    container: document.getElementById('aboutbm'),
@@ -23,18 +17,10 @@ var height = window.innerHeight ||
    path: "./aboutloop.json"
  });
 
-/*var aboutAnim = bodymovin.loadAnimation({
-  container = document.getElementById('aboutbm'),
-  renderer: "svg",
-  loop: true,
-  autoplay: true,
-  path: "./aboutloop.json"
-});*/
-
-//}
 document.addEventListener("DOMContentLoaded", function(event) {
   tl = new TimelineMax();
 
+  //INTRO ANIMATION
   var animationMain = bodymovin.loadAnimation({
     container: document.getElementById('bm2'), // Required
     path: './data.json', // Required
@@ -47,28 +33,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
   var select = function(s) {
       return document.querySelector(s);
     },
-    selectAll = function(s) {
-      return document.querySelectorAll(s);
-    },
-    animationWindow = select('aboutbm'),
-        animData = {
+  selectAll = function(s) {
+    return document.querySelectorAll(s);
+  },
+  animationWindow = select('aboutbm'),
+    animData = {
       wrapper: animationWindow,
       animType: 'svg',
       loop: false,
       prerender: true,
       autoplay: true,
       path: 'data.json',
-
     },
     anim, tl;
-
-
   anim = bodymovin.loadAnimation(animData);
-  //anim.addEventListener('DOMLoaded', onDOMLoaded);
   anim.setSpeed(1);
 
-
+//TIMELINE FOR THE SITE INTRO
   tl.to(("#cover"), 0.1, {autoAlpha:0})
+
   .from(("body"), 3, {
       backgroundColor: "#000000",
       ease: Power1.easeIn
@@ -80,45 +63,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
       x: "-500",
       y: "500"
     })
+
     .from(("#title"), 0.5, {x: "-50", autoAlpha:0})
 
-     .staggerFrom((".footer_li"), 0.5, {
+    .staggerFrom((".footer_li"), 0.5, {
        x: "-50",
        backgroundColor: "black",
        autoAlpha: 0.01
      }, 0.2)
-      .to({
-       frame: 0
-     }, 6.01, {
-       frame: anim.totalFrames - 1,
-       onUpdate: function() {
-         anim.goToAndStop(Math.round(this.target.frame), true)
-       },
-       ease: Linear.easeNone
-     });
 
-  //var scene = document.getElementsByClassName('rocket');
-  //var parallaxInstance = new Parallax(scene);
-
-
-//  console.log(anim.totalFrames);
-
+     .to({frame: 0},
+        6.01,
+        {frame: anim.totalFrames - 1,
+          onUpdate: function() {
+            anim.goToAndStop(Math.round(this.target.frame), true)
+          },ease: Linear.easeNone});
 });
 
-//document.getElementsByClassName("footer_li").addEventListener("mouseover", rollover);
-
-function rollover(){
-  //TweenMax.to()
-}
 document.getElementById("myWork").addEventListener("click", work);
-
 document.getElementById("aboutMe").addEventListener("click", about);
 document.ontouchmove = function (e) {
   e.preventDefault();
 }
 
 
-
+//FOOTER BUTTON WORK
 function work(){
   width = window.innerWidth ||
     document.documentElement.clientWidth ||
@@ -129,12 +98,9 @@ function work(){
     document.body.clientHeight;
 
 
-  if(document.getElementById("myWork").innerHTML == "WORK"){
-    console.log(homeTest);
+  if(document.getElementById("myWork").innerHTML == "WORK"){ //IF WORK MENU IS NOT TOGGLED
     document.getElementById("myWork").innerHTML="CLOSE";
     document.getElementById("aboutMe").innerHTML="ABOUT";
-
-
     TweenMax.to((".rocket"), 1.5, {
       ease: Power1.easeInOut,
       x: width / 2,
@@ -146,41 +112,23 @@ function work(){
     TweenMax.to(("#about"), 1, {autoAlpha:0});
 
 
-  }else if(document.getElementById("myWork").innerHTML == "CLOSE"){
+  }else if(document.getElementById("myWork").innerHTML == "CLOSE"){ //IF WORK MENU IS TOGGLED
     document.getElementById("myWork").innerHTML="WORK";
-    //document.getElementsByClassName("rocket").style.transform = "matrix(1, 0, 0, 1, -500, 500)";
-    TweenMax.to((".rocket"), 1.5, {
-      ease: Power1.easeInOut,
-      x: width / 2,
-      y: -height / 1.8,
-      autoAlpha: 1
-    });
     TweenMax.to(("#work"), 1, {autoAlpha:0});
-    TweenMax.to(("#bm2"), 1, {autoAlpha:1});
-
     document.getElementById("aboutMe").innerHTML="ABOUT";
-
-
   }
-
-
 };
-
+//FOOTER BUTTON ABOUT
 function about() {
-
     width = window.innerWidth ||
       document.documentElement.clientWidth ||
       document.body.clientWidth;
-
     height = window.innerHeight ||
       document.documentElement.clientHeight ||
       document.body.clientHeight;
 
-
-    if(document.getElementById("aboutMe").innerHTML == "ABOUT"){
-      console.log(homeTest);
+    if(document.getElementById("aboutMe").innerHTML == "ABOUT"){ //IF ABOUT MENU IS NOT TOGGLED
       document.getElementById("aboutMe").innerHTML="CLOSE";
-
       TweenMax.to((".rocket"), 1.5, {
         ease: Power1.easeInOut,
         x: width / 2,
@@ -190,12 +138,10 @@ function about() {
       TweenMax.to(("#about"), 1, {autoAlpha:1});
       TweenMax.to(("#work"), 1, {autoAlpha:0});
       document.getElementById("myWork").innerHTML="WORK";
-
-    }else if(document.getElementById("aboutMe").innerHTML == "CLOSE"){
+    }
+    else if(document.getElementById("aboutMe").innerHTML == "CLOSE"){ //IF ABOUT MENU IS TOGGLED
       document.getElementById("aboutMe").innerHTML="ABOUT";
       document.getElementById("myWork").innerHTML="WORK";
-
-      //document.getElementsByClassName("rocket").style.transform = "matrix(1, 0, 0, 1, -500, 500)";
       TweenMax.to(("#about"), 1, {autoAlpha:0});
       TweenMax.to(("#bm2"), 1, {autoAlpha:1});
 
